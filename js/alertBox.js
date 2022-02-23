7a4da18778d96294345cf17eb704b1e751b57b54
hee <monghee0727@gmail.com>
2022-02-23 16:57:35 +0900

    alertBox create
    complete

--- js/alertBox.js
+++ js/alertBox.js
@@ -1,56 +1,83 @@
 const loginBox = document.querySelector(".login-alert-box");
-const idBtn = document.querySelector("#login-id");
-const pwBtn = document.querySelector("#login-pw");
-const btn = document.querySelectorAll(".login-btn button");
+const loginBtn = document.querySelector("#login-btn");
+const popupWrap = document.querySelector("#popup-wrap");
 
 const HIDDEN_CLASSNAME = "hidden";
 
-/* login Box */
+/* 모달창 만들기 */
+const alertBox = document.createElement("div");
+alertBox.classList.add("alertBox");
+alertBox.classList.add(HIDDEN_CLASSNAME);
+const alertText = document.createElement("span");
+alertText.innerText =
+  "로그인 해라 이놈아, 로그인아이디랑 비밀번호 기억해내라 이놈아, 한번에 로그인 하도록!";
+const alertBtn = document.createElement("button");
+alertBtn.innerText = "Login Go";
+alertBtn.classList.add("btn");
+alertBtn.classList.add("btn-danger");
+alertBox.appendChild(alertText);
+alertBox.appendChild(alertBtn);
+popupWrap.appendChild(alertBox);
+
+/* login input */
 const loginForm = document.createElement("form");
-const loginInfo = document.createElement("input");
-loginForm.appendChild(loginInfo);
-loginInfo.classList.add("login-input");
-const submitBtn = document.createElement("button");
-loginForm.appendChild(submitBtn);
-submitBtn.classList.add("icon");
+loginForm.classList.add("login-form");
+const id = document.createElement("input");
+id.placeholder = "아이디를 입력해라";
+id.required = true;
+const pw = document.createElement("input");
+pw.placeholder = "비밀번호를 입력해라";
+pw.type = "password";
+pw.required = true;
+const submitLogin = document.createElement("button");
+submitLogin.innerText = "👍👍";
+submitLogin.classList.add("icon");
+
+loginForm.appendChild(id);
+loginForm.appendChild(pw);
+loginForm.appendChild(submitLogin);
 loginBox.appendChild(loginForm);
 
-function handleBtn() {
-  for (let i = 0; i < btn.length; i++) {
-    btn[i].classList.toggle(HIDDEN_CLASSNAME);
-  }
+/*  welcom content*/
+const welcome = document.createElement("h1");
+loginBox.after(welcome);
+welcome.classList.add("welcome-user");
+welcome.classList.add(HIDDEN_CLASSNAME);
+
+function paintUser(user) {
+  welcome.innerText = `Hello, ${user} :)`;
+  welcome.classList.toggle(HIDDEN_CLASSNAME);
 }
-function submitId(e) {
+
+function handlesubmitLogin(e) {
   e.preventDefault();
-  handleBtn();
-  const userId = loginInfo.value;
+  const userId = id.value;
+  const userPw = pw.value;
   localStorage.setItem("userId", userId);
-  loginBox.classList.toggle(HIDDEN_CLASSNAME);
-}
-function submitPw(e) {
-  e.preventDefault();
-  handleBtn();
-  const userPw = loginInfo.value;
   localStorage.setItem("userPw", userPw);
   loginBox.classList.toggle(HIDDEN_CLASSNAME);
+  paintUser(userId);
 }
 
-function handleBtnClick() {
-  loginInfo.required = true;
-  submitBtn.innerText = "👍";
+function alertBtnClick() {
   loginBox.classList.remove(HIDDEN_CLASSNAME);
-  submitBtn.addEventListener("submit", submitId);
-  handleBtn();
-}
-function idBtnClick() {
-  loginInfo.placeholder = "아이디를 입력해라";
-  handleBtnClick();
-}
-function pwBtnClick() {
-  loginInfo.placeholder = "비밀번호를 입력해라";
-  handleBtnClick();
+  popupWrap.classList.add(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", handlesubmitLogin);
+  alertBox.classList.toggle(HIDDEN_CLASSNAME);
 }
 
-loginBox.classList.add(HIDDEN_CLASSNAME);
-idBtn.addEventListener("click", idBtnClick);
-pwBtn.addEventListener("click", pwBtnClick);
+function loginClick() {
+  loginBtn.classList.add(HIDDEN_CLASSNAME);
+  popupWrap.classList.remove(HIDDEN_CLASSNAME);
+  alertBox.classList.remove(HIDDEN_CLASSNAME);
+  alertBtn.addEventListener("click", alertBtnClick);
+}
+
+const saveId = localStorage.getItem("userId");
+
+if (saveId !== null) {
+  paintUser(saveId);
+  loginBtn.classList.toggle(HIDDEN_CLASSNAME);
+} else {
+  loginBtn.addEventListener("click", loginClick);
+}
